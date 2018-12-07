a0fb749b86dd26476e3e98cded780a5b45772a42
Maxime <mlaniel@aptitudetech.net>
2018-12-07 13:53:06 -0500

    PirateDice-Working on business logics elements

--- src/piratedice/PirateDice.java
+++ src/piratedice/PirateDice.java
@@ -5,7 +5,11 @@
  */
 package piratedice;
 
+import BLL.Services;
+import Model.Card;
 import Model.Dice;
+import Model.Turn;
+import java.util.List;
 
 /**
  *
@@ -17,13 +21,40 @@ public class PirateDice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Dice d1 = new Dice();
-        Dice d2 = new Dice();
+
+        Services serv = Services.getInstance();
+
+        List<Dice> dicesGame = serv.startGame();
+
+        /*Testing Dices and states*/
+ /*
+        for (Dice dice : dicesGame) {
+            System.out.println(dice.getId() + "--" + dice.getState() + "--" + dice.getIsDeath());
+
+        }
+        System.out.println("");
+        serv.rollDices();
+
+        for (Dice dice : dicesGame) {
+            System.out.println(dice.getId() + "--" + dice.getState() + "--" + dice.getIsDeath());
+
+        }
+         */
+ /*Testing Cards*/
+ /*
+        for (Card card : serv.getAllCards()) {
+            System.out.println(card.getId() + "--" +card.getName());
+        }
+         */
+ /*Testing turn*/
+        Turn turn1 = new Turn(serv.getAllPlayers().get(0));
         
-        System.out.println(d1.getActualPosition());
-        System.out.println(d2.getActualPosition());
-        System.out.println(d2.rollDice());
+        serv.pickACard(turn1);
+        
+        System.out.println(turn1.getPlayer().getName() +" has picked the "+ turn1.getCard().getName() + " card!!!" );
+        
+        
         
     }
-    
+
 }
