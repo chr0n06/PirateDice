836d6b5720341d2f648cc8d9707247cea228e40a
chr0n06 <maxime.laniel@outlook.com>
2018-12-23 12:12:29 -0500

    PirateDice-Adding visual sequence of roll Dice

--- src/Controllers/BoardGame.java
+++ src/Controllers/BoardGame.java
@@ -1,13 +1,22 @@
+package Controllers;
 
+import Model.Dice;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class BoardGame implements Initializable {
 
-      @FXML
+    Dice_Controller dc = Dice_Controller.getInstance();
+
+    @FXML
     private ResourceBundle resources;
 
     @FXML
@@ -40,13 +49,24 @@ public class BoardGame implements Initializable {
     @FXML
     private ImageView dice9;
 
-    @FXML
-    void initialize() {
-  
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-           }
 
-}
+    }
+
+    @FXML
+    void onActionPlay(ActionEvent event) {
+        dc.rollAllDices();
+
+        dice1.setImage(new Image("Assets/" + dc.getAllDices().get(0).getState() + ".jpg"));
+        dice2.setImage(new Image("Assets/" + dc.getAllDices().get(1).getState() + ".jpg"));
+        dice3.setImage(new Image("Assets/" + dc.getAllDices().get(2).getState() + ".jpg"));
+        dice4.setImage(new Image("Assets/" + dc.getAllDices().get(3).getState() + ".jpg"));
+        dice5.setImage(new Image("Assets/" + dc.getAllDices().get(4).getState() + ".jpg"));
+        dice6.setImage(new Image("Assets/" + dc.getAllDices().get(5).getState() + ".jpg"));
+        dice7.setImage(new Image("Assets/" + dc.getAllDices().get(6).getState() + ".jpg"));
+        dice8.setImage(new Image("Assets/" + dc.getAllDices().get(7).getState() + ".jpg"));
+      
+    }//onActionPlay
+
+}//BoardGame
