5fe0720ee2e71265160c69683e982628c7d846bb
chr0n06 <maxime.laniel@outlook.com>
2018-12-23 00:38:58 -0500

    PirateDice-Adding primary FX View

---
+++ src/Controllers/BoardGame.java
@@ -0,0 +1,52 @@
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+public class BoardGame implements Initializable {
+
+      @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView dice1;
+
+    @FXML
+    private ImageView dice2;
+
+    @FXML
+    private ImageView dice3;
+
+    @FXML
+    private ImageView dice4;
+
+    @FXML
+    private ImageView dice5;
+
+    @FXML
+    private ImageView dice6;
+
+    @FXML
+    private ImageView dice7;
+
+    @FXML
+    private ImageView dice8;
+
+    @FXML
+    private ImageView dice9;
+
+    @FXML
+    void initialize() {
+  
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+           }
+
+}
---
+++ src/piratedice/PirateDiceGUI.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piratedice;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Maxime
+ */
+public class PirateDiceGUI extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/BoardGame.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        
+        stage.show();
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
